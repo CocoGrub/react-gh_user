1e18ed6f2fe4b3138a94e1313cb1e9b5832766f9
CocoGrub <fedsavin@gmail.com>
2020-12-05 07:54:48 +0300

    Updates

---
+++ precache-manifest.2a6c28db6946f7f62432f993a0735fc0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "084d7e3566728fcf659b136544b7e202",
+    "url": "/test-table/index.html"
+  },
+  {
+    "revision": "84dabd8c7173be76d44a",
+    "url": "/test-table/static/css/main.a365ad36.chunk.css"
+  },
+  {
+    "revision": "bba566f925489bbcd67c",
+    "url": "/test-table/static/js/2.27df8ec6.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/test-table/static/js/2.27df8ec6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84dabd8c7173be76d44a",
+    "url": "/test-table/static/js/main.e7cb5be7.chunk.js"
+  },
+  {
+    "revision": "8a544bbe4074866f3cd5",
+    "url": "/test-table/static/js/runtime-main.8ed38e40.js"
+  }
+]);
